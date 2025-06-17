d261b2640a1a13e25bc49774dcfbcf9f7fc2b13d
Felix Watts <felixwatts@gmail.com>
2025-06-17 08:51:20 +0100

    try fix value widget

--- src/view.rs
+++ src/view.rs
@@ -41,22 +41,6 @@ impl ValueWidgetTemplate{
             None => "No data".into()
         }
     }
-
-    pub fn view_box_width(&self) -> f32 {
-        self.config.width as f32 * 100.0
-    }
-
-    pub fn view_box_height(&self) -> f32 {
-        (self.config.height - 1) as f32 * 100.0
-    }
-
-    pub fn view_box_centre_x(&self) -> f32 {
-        self.view_box_width() / 2.0
-    }
-
-    pub fn view_box_centre_y(&self) -> f32 {
-        self.view_box_height() / 2.0
-    }
 }   
 
 #[derive(Template)]
